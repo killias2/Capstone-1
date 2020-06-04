70664395c6b6384592e015d6e93522539cd54fe2
Griffin van de Venne <vandevenne.griffin@gmail.com>
2020-06-04 15:02:59 -0400

    vending interface changes

--- src/main/java/com/techelevator/VendingInterface.java
+++ src/main/java/com/techelevator/VendingInterface.java
@@ -1,5 +1,6 @@
 package com.techelevator;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class VendingInterface {
@@ -57,7 +58,6 @@ public class VendingInterface {
 		}
 		
 	}
-	
 	private void purchaseMenu() {
 		System.out.println("(1) Feed Money");
 		System.out.println("(2) Select Product");
@@ -77,30 +77,59 @@ public class VendingInterface {
 		}
 		
 	}
-	private void feedMoneyMenu() {
+	private void feedMoneyMenu() { //completed
 		System.out.println("Current Balance: " + formatMoney(balance));
 		System.out.println("Please input bills to add more money (1, 2, 5, 10 are valid input types)");
-		System.out.println("Enter -1 to return to previous menu after bills have been added");
+		System.out.println("Enter 0 to return to previous menu after bills have been added");
 		String input = userInput.nextLine();
 		int numUserInput = Integer.parseInt(input);
-		if(numUserInput == -1) {
+		if(numUserInput == 0) {
 			menuNum = 1;
+			int[] change = makeChange();
+			System.out.println("You have been given " + change[0] + " quarters, " + change[1] + " dimes, and " + change[2] + " nickels.");
+			System.out.println("Current Balance: " + formatMoney(balance));
 		}
 		else if(numUserInput == 1 || numUserInput == 2 || numUserInput == 5 || numUserInput == 10) {
 			balance += numUserInput * 100;
 			purchaseLog.logFeed(formatMoney(numUserInput * 100), formatMoney(balance));
 			
 		}
-	
 		else{
 			System.out.println("Invalid input!");
 		}
 		
 	}
 	private void productSelectMenu() {
+		List<Product> productList = machine.getInventory();
+		for(Product p: productList) {
+			System.out.println(p.toString());
+		}
+		System.out.println("Enter a number of a product to buy!");
 		String input = userInput.nextLine();
 		int numUserInput = Integer.parseInt(input);
+		/*if (machine.productExists(numUserInput))
+		{
+			
+		}
+		else {
+			
+		}
+		if ()*/
+		//implement later once vending machine class works
 		
+	}
+	private int[] makeChange() { //return format is [quarters, dimes, nickels]
+		int[] rtn = new int[3];
+		purchaseLog.logChange(formatMoney(balance));
+		rtn[0] = balance / 25;
+		balance = balance % 25;
+		rtn[1] = balance / 10;
+		balance = balance % 10;
+		rtn[2] = balance / 5;
+		balance = balance % 5;
+		return rtn;
+		
+	
 	}
 	private String formatMoney(int i) {
 	int dollarAmt = i/ 100;
