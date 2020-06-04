12b54aa2526fd6832aaa66c7f25d3c0efc775f4b
Shane Killian <killias2@yahoo.com>
2020-06-04 16:08:45 -0400

    Added SalesReporter, for generating sales reports

--- src/main/java/com/techelevator/SalesReporter.java
+++ src/main/java/com/techelevator/SalesReporter.java
@@ -1,8 +1,13 @@
 package com.techelevator;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class SalesReporter {
 	
@@ -17,15 +22,63 @@ public class SalesReporter {
 			e.printStackTrace();
 		}
 		
-		List<Product> reportUpdates = new ArrayList<Product>();
+		int totalSales = 0;
+		Map<String,Integer> reportUpdates = new HashMap<String,Integer>();
 		
 		for (int i = 0; i < reportMachine.getSize(); i++) {
 			if (reportMachine.getProduct(i).getCount() < 5) {
-				reportUpdates.add(reportMachine.getProduct(i));
+				reportUpdates.put( reportMachine.getProduct(i).getName(),( 5 - reportMachine.getProduct(i).getCount() ) );
+				totalSales += (reportMachine.getProduct(i).getPrice() * (5 - reportMachine.getProduct(i).getCount() )) * 100;
 			}
 		}
 		
-		try ()
+		boolean checkTotalSales = false;
+		
+		try (Scanner reportReader = new Scanner(report); PrintWriter reportWriter = new PrintWriter(report)){
+			while(reportReader.hasNextLine()) {
+				String line = reportReader.nextLine();
+				String[] arrayLine = line.split("\\|");
+				if (reportUpdates.containsKey(arrayLine[0])){
+					int newCount = Integer.parseInt(arrayLine[1]) + reportUpdates.get(arrayLine[0]);
+					arrayLine[1] = String.valueOf(newCount);
+					line = String.join("\\|", arrayLine);
+					reportWriter.println(line);
+					reportUpdates.remove(arrayLine[0]);
+				}
+				if (line.contains("$")) {
+					double oldSales = Double.parseDouble(line.substring(1));
+					int oldSalesInt = (int)(oldSales * 100);
+					reportWriter.println(formatMoney(totalSales + oldSalesInt));
+					checkTotalSales = true;
+				}
+			}
+			
+			if(!reportUpdates.isEmpty()) {
+				try (PrintWriter logger = new PrintWriter(new FileWriter(report,true))){
+				for(Map.Entry<String, Integer> item : reportUpdates.entrySet()) {
+					reportWriter.println(item.getKey() + "\\|" + item.getValue());
+				}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (checkTotalSales = false) {
+				reportWriter.println(formatMoney(totalSales));
+			}
+			
+			} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
+		
+	private String formatMoney(int i) {
+		int dollarAmt = i/ 100;
+		int centsAmt = i % 100;
+		if(centsAmt % 10 == 0)
+		{
+			return "$" + dollarAmt + "." + centsAmt + "0";
+		}
+		return "$" + dollarAmt + "." + centsAmt;
+		}
 
 }
