8eb7bb5bf4598c4d35066276c11946fbe143f500
Shane Killian <killias2@yahoo.com>
2020-06-05 11:57:48 -0400

    Fixed Formatting and Math Issues With SalesReporter, Broke Program Down Into Methods

--- src/main/java/com/techelevator/SalesReporter.java
+++ src/main/java/com/techelevator/SalesReporter.java
@@ -11,20 +11,11 @@ import java.util.Scanner;
 
 public class SalesReporter {
 	File report = new File("SalesReport.txt");
+	 int totalSales = 0;
+	 int oldSalesInt = 0;
+	 boolean moveSales = false;
 	
-	public void updateReport(VendingMachine reportMachine) {
-		File updatedReport = new File("SalesReportTemp.txt");
-		
-		try {
-			if (!report.exists()) {
-				report.createNewFile();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		int totalSales = 0;
-		int oldSalesInt = 0;
+	private Map<String,Integer> generateMap(VendingMachine reportMachine) {
 		Map<String,Integer> reportUpdates = new HashMap<String,Integer>();
 		
 		for (int i = 0; i < reportMachine.getSize(); i++) {
@@ -33,9 +24,10 @@ public class SalesReporter {
 				totalSales += (reportMachine.getProduct(i).getPrice() * (5 - reportMachine.getProduct(i).getCount() ));
 			}
 		}
-		
-		boolean moveSales = false;
-		
+		return reportUpdates;
+	}
+	
+	private void writeReport1(File report, File updatedReport, Map<String,Integer> reportUpdates) {
 		try (Scanner reportReader = new Scanner(report);
 				PrintWriter reportWriter = new PrintWriter(updatedReport)){
 			updatedReport.createNewFile();
@@ -43,6 +35,9 @@ public class SalesReporter {
 				String line = reportReader.nextLine();
 				String[] arrayLine = line.split("\\|");
 				
+				if (!reportUpdates.containsKey(arrayLine[0]) && (!line.contains("$")) && (!line.equals(""))) {
+					reportWriter.println(line);
+				}
 				if (reportUpdates.containsKey(arrayLine[0])){
 					int newCount = Integer.parseInt(arrayLine[1]) + reportUpdates.get(arrayLine[0]);
 					arrayLine[1] = String.valueOf(newCount);
@@ -52,10 +47,12 @@ public class SalesReporter {
 				}
 				
 				if (line.contains("$")) {
-					double oldSales = Double.parseDouble(line.substring(1));
-					oldSalesInt = (int)(oldSales * 100);
-					line = formatMoney(totalSales + (oldSalesInt));
+					String oldSalesNewString = line.substring(1,line.length() - 3) + line.substring(line.length() - 2);
+					int oldSalesInt = Integer.parseInt(oldSalesNewString);
+					totalSales += oldSalesInt;
+					line = formatMoney(totalSales);
 					if (reportUpdates.isEmpty()) {
+						reportWriter.println();
 						reportWriter.println(line);
 					}
 				}
@@ -66,7 +63,10 @@ public class SalesReporter {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-			
+	}
+		
+		
+	private void writeReport2(File report, File updatedReport, Map<String,Integer> reportUpdates) {	
 		try (Scanner reportReader = new Scanner(updatedReport);
 				PrintWriter reportWriter = new PrintWriter(report)){
 			while(reportReader.hasNextLine()) {
@@ -97,10 +97,24 @@ public class SalesReporter {
 				e.printStackTrace();
 			}
 		}
-		
-		
 	}
 	
+	public void updateReport(VendingMachine reportMachine) {
+		File updatedReport = new File("SalesReportTemp.txt");
+		Map<String,Integer> reportUpdates = generateMap(reportMachine);
+		
+		try {
+			if (!report.exists()) {
+				report.createNewFile();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		writeReport1(report,updatedReport,reportUpdates);
+		writeReport2(report,updatedReport,reportUpdates);
+		
+	}
 		
 	private String formatMoney(int i) {
 		int dollarAmt = i/ 100;
@@ -109,23 +123,40 @@ public class SalesReporter {
 		{
 			return "$" + dollarAmt + "." + centsAmt + "0";
 		}
-		return "$" + dollarAmt + "." + centsAmt;
+		else if(centsAmt < 10) {
+			return "$" + dollarAmt + "." + "0" + centsAmt;
 		}
+		return "$" + dollarAmt + "." + centsAmt;
+	}
 	
-	public void generateReport() {
+	public void generateReport(VendingMachine reportMachine) {
 		String generatedPath = java.time.LocalDate.now() + "_" + java.time.LocalTime.now()  + "_SalesReport.txt";
 		File generatedReport = new File(generatedPath);
+		Map<String,Integer> reportUpdates = generateMap(reportMachine);
 		
-		try (Scanner reportReader = new Scanner(report);
-				PrintWriter reportWriter = new PrintWriter(generatedReport)){
-			while(reportReader.hasNextLine()) {
-				String line = reportReader.nextLine();
-				reportWriter.println(line);
+		writeReport1(report, generatedReport,reportUpdates);
+		
+		if(!reportUpdates.isEmpty()) {
+			
+			try (PrintWriter reportWriter = new PrintWriter(new FileWriter(generatedReport,true))){
+				
+				for(Map.Entry<String, Integer> item : reportUpdates.entrySet()) {
+					reportWriter.println(item.getKey() + "|" + item.getValue());
+				}
+				
+				if (moveSales == false) {
+					reportWriter.println();
+					reportWriter.println(formatMoney(totalSales + oldSalesInt));
+				}
+				
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		}
+		totalSales = 0;
+		oldSalesInt = 0;
+		moveSales = false;
+		
 	}
 
 }
